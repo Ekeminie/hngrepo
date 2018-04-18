1fc7b4863a01d92c0319934296d050ea2f4854de
Ekeminie <mikeblize@gmail.com>
2018-04-18 19:46:30 +0100

    Add Hng.php

---
+++ Hng.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <link rel="stylesheet" href="css/Hng.css">
+<style>
+h1{font-family: Verdana, Geneva, Tahoma, sans-serif;
+font-size: 250%;
+padding-top: 80px;
+}
+div {padding-top: 150px;
+font-size: 400%;
+color: darkturquoise;
+text-align: right;
+margin-right: 200px  
+}
+  
+</style>
+    <title>HNG INTERNSHIP</title>
+
+  </head>
+  <body>
+
+        <h1 align="center">HNG INTERNSHIP 4.0</h1>
+
+      <div>
+      <?php date_default_timezone_set("Africa/Lagos");?>  
+      <?php echo date ("h:i:sa"); ?>
+      
+    
+        </div>
+
+        </body>
+        </html>
